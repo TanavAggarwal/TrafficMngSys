db6315665124c3b24072bf09805493c0b7b3acb7
Tanav Aggarwal <55316906+TanavAggarwal@users.noreply.github.com>
2020-10-07 23:50:30 +0530

    Update TrafficMng.cpp

--- TrafficMng.cpp
+++ TrafficMng.cpp
@@ -1,95 +1,52 @@
 #include <iostream>
 #include<queue>
-#include<vector>
 #include<string>
 #include<fstream>
+#include<vector>
 using namespace std;
 
 class Authorisation {
-    public:
-    int auth(){
-	label:
-	string p,q,w;
-	int x;
-    string s;
-	cin>>x;
-	if(x==1){
-
-			
-  			ofstream fout;
-  			cout<<"ENTER YOUR USERNAME"<<endl;
-  			cin>>s;
-  			cout<<"ENTER YOUR PASSWORD"<<endl;
-  			cin>>p;
-  			s=s+p;
-  			ifstream fin;
-  			string line;
-  			int offset=0;
-  			fin.open("myfile.txt");
-  			if(fin.is_open())
-  			{
-  				while(!fin.eof())
-  				{
-  					getline(fin,line);
-					if(line.find(s,0)!=-1)
-					{
-						cout<<"sorry! this username is not available"<<endl;
-						cout<<"Press any key to go to Home Page"<<endl;
-						char kk;
-						cin>>kk;
-						goto label;
-						//break;
-					}
-				}
-			}
-  			fin.close();
-  			fout.open("myfile.txt",ios::app);
-  			fout<<s+"\n";
-  			fout.close();
-	}
-		else if(x==2){
-
-			string line;
-			
-  			ifstream fin;
-  			int offset=0;
-  			cout<<"ENTER YOUR USERNAME"<<endl;
-  			cin>>q;
-  			cout<<"ENTER YOUR PASSWORD"<<endl;
-  			cin>>w;
-  			q=q+w;
-  			fin.open("myfile.txt");
-  			if(fin.is_open())
-  			{
-  				while(!fin.eof())
-  				{
-  					getline(fin,line);
-					if(line.find(q,0)!=-1)
-					{
-						cout<<"you are logged in"<<endl;
-						offset=1;
-						break;
-						  }
-				}
-				if(offset==0)
-				{
-					cout<<"sorry you are unauthorised!!"<<endl;
-					cout<<"Press 1 and enter to go to home page"<<endl;
-					//getch();
-					char o;
-					cin>>o;
-					goto label;
-								  }
-			}
-  			fin.close();
-	}
-	else if(x==3){
-		return 0;
-	}else{
-	    goto label;
-	}
-    return 0;
-}
+public:
+    void auth() {
+        int c;
+        l1:cout << "Enter 1 to register / 2 to login : ";
+        cin >> c;
+        cout << endl;
+        if (c == 1) {
+            ofstream op("User_Database.txt", ios::app);
+            cout << "ENTER DETAILS TO REGISTER YOURSELF - " << endl;
+            string user, pass;
+            cout << "Enter username : ";
+            cin >> user;
+            cout << "Enter password : ";
+            cin >> pass;
+            op << user << " " << pass << endl;
+            op.close();
+            cout << "Registered Successfully!" << endl << endl;
+            goto l1;
+        }
+        else if (c == 2) {
+            cout << "ENTER DETAILS TO LOGIN - " << endl;
+            string user, pass, uf, pf;
+            cout << "Enter username : ";
+            cin >> user;
+            cout << "Enter password : ";
+            cin >> pass;
+            ifstream ip("User_Database.txt");
+            while (ip >> uf >> pf) {
+                if (uf == user && pf == pass) {
+                    cout << "\nWelcome " << user << ", Login Successfull!" << endl << endl;
+                    return;
+                }
+            }
+            cout << "Login Unsuccessfull! Try Again!" << endl << endl;
+            goto l1;
+        }
+        else {
+            cout << "Enter a valid choice !" << endl;
+            goto l1;
+        }
+    }
 };
 
 class Alerts {
@@ -201,13 +158,16 @@ public:
 };
 
 int main() {
-    cout << "** TRAFFIC MANAGEMENT SYSTEM **" << endl << endl;
+    cout << "**********                         **********" << endl << endl;
+    cout << "          TRAFFIC MANAGEMENT SYSTEM          " << endl << endl;
+    cout << "**********                         **********" << endl << endl;
     cout << "Authorise yourself to start simulation -" << endl;
-    l1:cout << "Press 1 to register OR Press 2 to login : ";
-    
     Authorisation AU;
     AU.auth();
-    cout << "\n\n********************************\n\n";
+    cout << "***********************************************" << endl;
+    cout << "Press ENTER to continue........"; cin.get(); cin.get();
+    system("cls");
 
+    
     return 0;
 }
